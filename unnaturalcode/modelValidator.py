29b40f116a4c159c377ce52591d9d3be9267e631
Joshua Charles Campbell <joshua2@ualberta.ca>
2017-03-09 16:05:39 -0700

    refactoring completeish

--- unnaturalcode/modelValidator.py
+++ unnaturalcode/modelValidator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#    Copyright 2013, 2014 Joshua Charles Campbell
+#    Copyright 2013, 2014, 2017 Joshua Charles Campbell
 #
 #    This file is part of UnnaturalCode.
 #    
@@ -31,17 +31,10 @@ import logging
 from os import path
 
 import csv
-import runpy
-import sys, traceback
 from shutil import copyfile
 from tempfile import mkstemp, mkdtemp
 import os, re, site
 
-from multiprocessing import Process, Queue
-try:
-  from Queue import Empty
-except ImportError:
-  from queue import Empty
 from unnaturalcode import flexibleTokenize
 
 import pdb
@@ -61,83 +54,8 @@ class HaltingError(Exception):
   def __str__(self):
     return repr(self.value)
 
-def runFile(q,path,mode):
-    if not virtualEnvActivate is None:
-      if sys.version_info >= (3,0):
-        exec(compile(open(virtualEnvActivate, "rb").read(), virtualEnvActivate, 'exec'), dict(__file__=virtualEnvActivate))
-      else:
-        execfile(virtualEnvActivate, dict(__file__=virtualEnvActivate))
-    parent = path
-    runit = None
-    while len(parent) > 1:
-        parent = os.path.dirname(parent)
-        sys.path = sys.path + [parent]
-        if mode == 'module':
-            moduleSite = ""
-            #info("Path: %s" % path)
-            for s in sys.path:
-                if s in path:
-                    if ('site-packages' in path and
-                        (not 'site-packages' in os.path.basename(s))):
-                        continue
-                    if len(s) > len(moduleSite):
-                        moduleSite = s
-                        break
-            #info("Python path: %s" % moduleSite)
-            relpath = os.path.relpath(path, moduleSite)
-            #info("Relative path: %s" % relpath)
-            components = relpath.split(os.path.sep)
-            components[-1] = components[-1].replace(".py", "", 1)
-            module = ".".join(components)
-            #info("Module name: %s" % module)
-            runit = lambda: runpy.run_module(module)
-        elif mode == 'script':
-            runit = lambda: runpy.run_path(path)
-        elif mode == 'module_indir':
-            moduledir = os.path.dirname(path)
-            filename = os.path.basename(path)
-            os.chdir(moduledir)
-            runit = lambda: runpy.run_path(filename)
-        else:
-            raise ValueError("Mode not recognized?") 
-    old_stdout = os.dup(sys.stdout.fileno())
-    old_stderr = os.dup(sys.stderr.fileno())
-    old_stdin = os.dup(sys.stdin.fileno())
-    devnull = os.open('/dev/null', os.O_RDWR)
-    os.dup2(devnull, sys.stdout.fileno())
-    os.dup2(devnull, sys.stderr.fileno())
-    os.dup2(devnull, sys.stdin.fileno())
-    try:
-        runit()
-    except SyntaxError as se:
-        os.dup2(old_stdout, sys.stdout.fileno())
-        os.dup2(old_stderr, sys.stderr.fileno())
-        os.dup2(old_stdin, sys.stdin.fileno())
-        ei = sys.exc_info();
-        #info("run_path exception:", exc_info=ei)
-        eip = (ei[0], str(ei[1]), traceback.extract_tb(ei[2]))
-        try:
-          eip[2].append(ei[1][1])
-        except IndexError:
-          eip[2].append((se.filename, se.lineno, None, None))
-        q.put(eip)
-        return
-    except Exception as e:
-        os.dup2(old_stdout, sys.stdout.fileno())
-        os.dup2(old_stderr, sys.stderr.fileno())
-        os.dup2(old_stdin, sys.stdin.fileno())
-        ei = sys.exc_info();
-        #info("run_path exception:", exc_info=ei)
-        eip = (ei[0], str(ei[1]), traceback.extract_tb(ei[2]))
-        q.put(eip)
-        return
-    finally:
-        os.dup2(old_stdout, sys.stdout.fileno())
-        os.dup2(old_stderr, sys.stderr.fileno())
-        os.dup2(old_stdin, sys.stdin.fileno())
-    q.put((None, "None", [(path, None, None, None)]))
     
-class validationFile(object):
+class ValidationFile(object):
     
     def __init__(self, path, language, tempDir):
         self.path = path
@@ -150,33 +68,6 @@ class validationFile(object):
         self.mutatedLexemes = None
         self.mutatedLocation = None
         self.tempDir = tempDir
-        self.mode = 'script'
-        r = self.run(path)
-        rscript = r
-        if (r[0] != None):
-            self.mode = 'module'
-            r = self.run(path)
-            if (r[0] != None):
-                self.mode = 'module_indir'
-                r = self.run(path)
-        #info("Ran %s as a %s, got %s" % (self.path, self.mode, r[1]))
-        if (r[0] != None):
-          raise Exception("Couldn't run file: %s because %s" % (self.path, r[1]))
-    
-    def run(self, path):
-        q = Queue()
-        p = Process(target=runFile, args=(q,path,self.mode))
-        p.start()
-        try:
-          r = q.get(True, 10)
-        except Empty as e:
-          r = (HaltingError, "Didn't halt.", [(path, None, None, None)])
-        p.terminate()
-        p.join()
-        assert not p.is_alive()
-        #assert r[2][-1][2] != "_get_code_from_file" # This seems to be legit
-        return r
-
     
     def mutate(self, lexemes, location):
         assert isinstance(lexemes, ucSource)
@@ -192,7 +83,7 @@ class validationFile(object):
         os.remove(mutantFilePath)
         return r
         
-class modelValidation(object):
+class ModelValidation(object):
     
     def addValidationFile(self, files):
           """Add a file for validation..."""
@@ -202,7 +93,7 @@ class modelValidation(object):
           nAdded = 0
           for fi in files:
             try:
-                vfi = validationFile(fi, self.lm, self.resultsDir)
+                vfi = self.languageValidationFile(fi, self.lm, self.resultsDir)
                 if len(vfi.lexed) > self.sm.windowSize:
                     self.validFiles.append(vfi)
                     info("Using %s in %s mode." % (fi, vfi.mode))
@@ -225,7 +116,7 @@ class modelValidation(object):
         n_so_far = 0
         assert n > 0
         for fi in self.validFiles:
-          assert isinstance(fi, validationFile)
+          assert isinstance(fi, ValidationFile)
           if fi.path in self.progress:
             progress = self.progress[fi.path]
           else:
@@ -236,12 +127,7 @@ class modelValidation(object):
             if merror is not None:
               info(merror)
               break
-            runException = fi.runMutant()
-            if (runException[0] == None):
-              exceptionName = "None"
-            else:
-              exceptionName = runException[0].__name__
-            filename, line, func, text = runException[2][-1]
+            filename, line, func, text, exceptionName = self.get_error(fi)
             if (fi.mutatedLocation.start.line == line):
               online = True
             else:
@@ -285,7 +171,11 @@ class modelValidation(object):
             info("MRR %f MR %f M5+ %f" % (mrr, mr, mtn))
             
       
-    def __init__(self, source=None, language=pythonSource, resultsDir=None, corpus=mitlmCorpus):
+    def __init__(self, 
+                 source=None, 
+                 language=pythonSource, 
+                 resultsDir=None,
+                 corpus=mitlmCorpus):
         self.resultsDir = ((resultsDir or os.getenv("ucResultsDir", None)) or mkdtemp(prefix='ucValidation-'))
         if isinstance(source, str):
             raise NotImplementedError
@@ -327,44 +217,41 @@ class modelValidation(object):
         """I am a destructor, but release should be called explictly."""
         assert not self.cm, "Destructor called before release()"
 
-def main():
+class ValidationMain(object):
+  
+    def add_args(self, parser):
+        """For overriding in subclasses to add more args."""
+        pass 
+    
+    def read_args(self, args):
+        """For overriding in subclasses to add more args."""
+        pass
+   
+    def main(self):
         from argparse import ArgumentParser
         parser = ArgumentParser(description="Test and Valide UnnaturalCode")
         parser.add_argument('-t', '--test-file-list', nargs='?', help='List of files to Test')
         parser.add_argument('-n', '--iterations', type=int, help='Number of times to iterate', default=50)
         parser.add_argument('-o', '--output-dir', help='Location to store output files', default='.')
         parser.add_argument('-m', '--mutation', help='Mutation to use')
-        parser.add_argument('-v', '--virtualenv', help='VirtualEnv to use when running the files under test. Must be the location of activate_this.py')
         parser.add_argument('-M', '--mitlm', help='Location of MITLM binary')
-        parser.add_argument('-T', '--retrain', action='store_true', help='Retrain model first')
+        self.add_args(parser) # get more args from subclasses
         args=parser.parse_args()
         logging.getLogger().setLevel(logging.DEBUG)
         testFileList = args.test_file_list
         testProjectFiles = open(testFileList).read().splitlines()
-        global virtualEnvActivate
-        virtualEnvActivate = args.virtualenv
-        # wow, this is actually how virtualenv does it...
-        virtualEnvBase = os.path.basename(os.path.basename(virtualEnvActivate))
         
         if (args.mitlm):
             os.environ["ESTIMATENGRAM"] = args.mitlm
             os.environ["LD_LIBRARY_PATH"] = os.path.dirname(args.mitlm)
             error(os.environ["LD_LIBRARY_PATH"])
         
-        if (args.retrain):
-            ucpy = pyUser()
-            ucpy.delete()
-            ucpy.sm.trainFile(testProjectFiles)
-        
-        virtualEnvSite = os.path.join(virtualEnvBase, 'lib', 'python%s' % sys.version[:3], 'site-packages')
-        v = modelValidation(source=testProjectFiles, 
-                            language=pythonSource, 
+        self.read_args(args)
+        v = self.validation(source=testProjectFiles, 
                             corpus=mitlmCorpus, 
                             resultsDir=args.output_dir)
+        
         v.validate(mutation=getattr(Mutators, args.mutation), n=args.iterations)
         # TODO: assert csvs
         v.release()
-        
 
-if __name__ == '__main__':
-    main()
---
+++ unnaturalcode/pythonValidator.py
@@ -0,0 +1,180 @@
+#!/usr/bin/python
+#    Copyright 2013, 2014, 2017 Joshua Charles Campbell
+#
+#    This file is part of UnnaturalCode.
+#    
+#    UnnaturalCode is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    UnnaturalCode is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with UnnaturalCode.  If not, see <http://www.gnu.org/licenses/>.
+
+from logging import debug, info, warning, error
+import logging
+import os
+import sys
+from multiprocessing import Process, Queue
+try:
+  from Queue import Empty
+except ImportError:
+  from queue import Empty
+import sys
+import traceback
+import runpy
+
+
+from unnaturalcode.modelValidator import ValidationFile, ModelValidation, ValidationMain
+from unnaturalcode.mitlmCorpus import mitlmCorpus
+from unnaturalcode.pythonSource import pythonSource
+from unnaturalcode.mutators import Mutators
+
+def runFile(q,path,mode):
+    if not virtualEnvActivate is None:
+      if sys.version_info >= (3,0):
+        exec(compile(open(virtualEnvActivate, "rb").read(), virtualEnvActivate, 'exec'), dict(__file__=virtualEnvActivate))
+      else:
+        execfile(virtualEnvActivate, dict(__file__=virtualEnvActivate))
+    parent = path
+    runit = None
+    while len(parent) > 1:
+        parent = os.path.dirname(parent)
+        sys.path = sys.path + [parent]
+        if mode == 'module':
+            moduleSite = ""
+            #info("Path: %s" % path)
+            for s in sys.path:
+                if s in path:
+                    if ('site-packages' in path and
+                        (not 'site-packages' in os.path.basename(s))):
+                        continue
+                    if len(s) > len(moduleSite):
+                        moduleSite = s
+                        break
+            #info("Python path: %s" % moduleSite)
+            relpath = os.path.relpath(path, moduleSite)
+            #info("Relative path: %s" % relpath)
+            components = relpath.split(os.path.sep)
+            components[-1] = components[-1].replace(".py", "", 1)
+            module = ".".join(components)
+            #info("Module name: %s" % module)
+            runit = lambda: runpy.run_module(module)
+        elif mode == 'script':
+            runit = lambda: runpy.run_path(path)
+        elif mode == 'module_indir':
+            moduledir = os.path.dirname(path)
+            filename = os.path.basename(path)
+            os.chdir(moduledir)
+            runit = lambda: runpy.run_path(filename)
+        else:
+            raise ValueError("Mode not recognized?") 
+    old_stdout = os.dup(sys.stdout.fileno())
+    old_stderr = os.dup(sys.stderr.fileno())
+    old_stdin = os.dup(sys.stdin.fileno())
+    devnull = os.open('/dev/null', os.O_RDWR)
+    os.dup2(devnull, sys.stdout.fileno())
+    os.dup2(devnull, sys.stderr.fileno())
+    os.dup2(devnull, sys.stdin.fileno())
+    try:
+        runit()
+    except SyntaxError as se:
+        os.dup2(old_stdout, sys.stdout.fileno())
+        os.dup2(old_stderr, sys.stderr.fileno())
+        os.dup2(old_stdin, sys.stdin.fileno())
+        ei = sys.exc_info();
+        #info("run_path exception:", exc_info=ei)
+        eip = (ei[0], str(ei[1]), traceback.extract_tb(ei[2]))
+        try:
+          eip[2].append(ei[1][1])
+        except IndexError:
+          eip[2].append((se.filename, se.lineno, None, None))
+        q.put(eip)
+        return
+    except Exception as e:
+        os.dup2(old_stdout, sys.stdout.fileno())
+        os.dup2(old_stderr, sys.stderr.fileno())
+        os.dup2(old_stdin, sys.stdin.fileno())
+        ei = sys.exc_info();
+        #info("run_path exception:", exc_info=ei)
+        eip = (ei[0], str(ei[1]), traceback.extract_tb(ei[2]))
+        q.put(eip)
+        return
+    finally:
+        os.dup2(old_stdout, sys.stdout.fileno())
+        os.dup2(old_stderr, sys.stderr.fileno())
+        os.dup2(old_stdin, sys.stdin.fileno())
+    q.put((None, "None", [(path, None, None, None)]))
+
+class PythonValidationFile(ValidationFile):
+    def run(self, path):
+        q = Queue()
+        p = Process(target=runFile, args=(q,path,self.mode))
+        p.start()
+        try:
+          r = q.get(True, 10)
+        except Empty as e:
+          r = (HaltingError, "Didn't halt.", [(path, None, None, None)])
+        p.terminate()
+        p.join()
+        assert not p.is_alive()
+        #assert r[2][-1][2] != "_get_code_from_file" # This seems to be legit
+        return r
+  
+    def __init__(self, path, language, tempDir):
+        super(PythonValidationFile,self).__init__(path, language, tempDir)
+        self.mode = 'script'
+        r = self.run(path)
+        rscript = r
+        if (r[0] != None):
+            self.mode = 'module'
+            r = self.run(path)
+            if (r[0] != None):
+                self.mode = 'module_indir'
+                r = self.run(path)
+        #info("Ran %s as a %s, got %s" % (self.path, self.mode, r[1]))
+        if (r[0] != None):
+          raise Exception("Couldn't run file: %s because %s" % (self.path, r[1]))
+
+class PythonValidation(ModelValidation):
+    def get_error(self, fi):
+      runException = fi.runMutant()
+      if (runException[0] == None):
+        exceptionName = "None"
+      else:
+        exceptionName = runException[0].__name__
+      filename, line, func, text = runException[2][-1]
+      return (filename, line, func, text, exceptionName)
+
+    def __init__(self, 
+                 source=None, 
+                 resultsDir=None,
+                 corpus=mitlmCorpus):
+       self.languageValidationFile = PythonValidationFile
+       super(PythonValidation,self).__init__(source=source,
+                                             language=pythonSource,
+                                             resultsDir=resultsDir,
+                                             corpus=mitlmCorpus)
+
+class PythonValidationMain(ValidationMain):
+    def add_args(self, parser):
+        parser.add_argument('-v', '--virtualenv', help='VirtualEnv to use when running the files under test. Must be the location of activate_this.py')
+
+    def read_args(self, args):
+        global virtualEnvActivate
+        virtualEnvActivate = args.virtualenv
+        # wow, this is actually how virtualenv does it...
+        virtualEnvBase = os.path.basename(os.path.basename(virtualEnvActivate))
+        virtualEnvSite = os.path.join(virtualEnvBase, 'lib', 'python%s' % sys.version[:3], 'site-packages')
+
+    def __init__(self, *args, **kwargs):
+        self.validation = PythonValidation
+        super(PythonValidationMain,self).__init__(*args, **kwargs)
+
+if __name__ == '__main__':
+    PythonValidationMain().main()
