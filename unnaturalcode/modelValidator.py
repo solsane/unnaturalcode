8667338b1d148a8173a7da820af6d70b05b8861b
Joshua Charles Campbell <joshua2@ualberta.ca>
2017-04-06 16:04:53 -0600

    typos

--- unnaturalcode/modelValidator.py
+++ unnaturalcode/modelValidator.py
@@ -198,11 +198,11 @@ class ModelValidation(object):
                 else:
                     fix = "ValidFix"
                     valids += 1
-                if fixop = "Delete":
+                if fixop == "Delete":
                     deletions += 1
-                elif fixop = "Insert"
+                elif fixop == "Insert"
                     insertions += 1
-                elif fixop = "Replace"
+                elif fixop == "Replace"
                     substitutions += 1
             info(fix + " " + fixop)
             if uc_result >= len(worst):
